5ea72607e51966eb95bd1a53dec52cd2fd6eb194
134765NicholasMunene <143217668+134765NicholasMunene@users.noreply.github.com>
2023-11-17 11:46:08 +0300

    update

---
+++ venv/Chatbot.py
@@ -0,0 +1,23 @@
+# bot.py
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from cleaner import remove_chat_metadata, remove_non_message_text
+
+from cleaner import clean_corpus
+
+CORPUS_FILE = "chat.txt"
+
+chatbot = ChatBot("Chatpot")
+
+trainer = ListTrainer(chatbot)
+cleaned_corpus = clean_corpus(CORPUS_FILE)
+trainer.train(cleaned_corpus)
+
+exit_conditions = (":q", "quit", "exit")
+while True:
+    query = input("> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"🪴 {chatbot.get_response(query)}")
